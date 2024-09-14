22e066b699e7c171eb0bd6e06e25a94a095cabf9
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-14 21:48:20 +0200

    edit export component to default

--- src/components/WorkflowSection.jsx
+++ src/components/WorkflowSection.jsx
@@ -4,7 +4,7 @@ import { CheckCircle2 } from 'lucide-react'
 import codeImg from '../assets/code.jpg'
 import { checklistItems } from '../constants'
 
-export const WorkflowSection = () => {
+const WorkflowSection = () => {
 	return (
 		<div className='mt-20'>
 			<h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>
@@ -38,3 +38,5 @@ export const WorkflowSection = () => {
 		</div>
 	)
 }
+
+export default WorkflowSection
