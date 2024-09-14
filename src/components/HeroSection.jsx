b0cc753703c03d20d2eb91e44e098b03ad527fa6
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-14 20:04:45 +0200

    add new feature section and edit hero section video display options

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
 					autoPlay
 					loop
 					muted
-					className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+					className='hidden md:block rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
 					<source src={video1} type='video/mp4' />
 					Your browser does not support the video tag.
 				</video>
@@ -41,7 +41,7 @@ const HeroSection = () => {
 					autoPlay
 					loop
 					muted
-					className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+					className='rounded-lg w-full md:w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
 					<source src={video2} type='video/mp4' />
 					Your browser does not support the video tag.
 				</video>
